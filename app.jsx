58106587d32cf32f9c10e3ad765e808024b89645
J. Ryan Conklin <jryanconklin@gmail.com>
2017-01-16 13:43:44 -0800

    Create State for Counter and Refactor

--- app.jsx
+++ app.jsx
@@ -30,19 +30,25 @@ Header.propTypes = {
   title: React.PropTypes.string.isRequired,
 };
 
-function Counter(props) {
-  return (
-    <div className="counter">
-      <button className="counter-action decrement"> - </button>
-      <div className="counter-score"> {props.score} </div>
-      <button className="counter-action increment"> + </button>
-    </div>
-  );
-}
+var Counter = React.createClass({
+  propTypes: {},
 
-Counter.propTypes = {
-  score: React.PropTypes.number.isRequired,
-}
+  getInitialState: function() {
+    return {
+      score: 0,
+    }
+  },
+
+  render: function() {
+    return (
+      <div className="counter">
+        <button className="counter-action decrement"> - </button>
+        <div className="counter-score"> {this.state.score} </div>
+        <button className="counter-action increment"> + </button>
+      </div>
+    );
+  }
+});
 
 function Player(props) {
   return (
@@ -51,7 +57,7 @@ function Player(props) {
         {props.name}
       </div>
       <div className="player-score">
-        <Counter score={props.score} />
+        <Counter />
       </div>
     </div>
   );
