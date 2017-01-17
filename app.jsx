94df2bb0d1230608d45a11b33c3ceabe21753546
J. Ryan Conklin <jryanconklin@gmail.com>
2017-01-16 16:20:47 -0800

    Add the Add Player Form

--- app.jsx
+++ app.jsx
@@ -3,24 +3,64 @@
 var PLAYERS = [
   {
     name: "Joe Person",
-    score: 31,
-    id: 1,
+    score: 0,
+    id: 0,
   },
   {
     name: "Another Person",
-    score: 25,
-    id: 2,
+    score: 0,
+    id: 1,
   },
   {
     name: "More Persons",
-    score: 26,
-    id: 3,
+    score: 0,
+    id: 2,
   }
 ];
+
+var AddPlayerForm = React.createClass({
+  render: function() {
+    return (
+      <div className="add-player-form">
+        <form>
+          <input type="text" />
+          <input type="submit" value="Add Player" />
+        </form>
+      </div>
+    )
+  }
+});
+
+function Stats(props) {
+  var totalPlayers = props.players.length;
+  var totalPoints = props.players.reduce(function(total, player) {
+    return total + player.score;
+  }, 0);
+
+  return (
+    <table className="stats">
+      <tbody>
+        <tr>
+          <td>Players:</td>
+          <td>{totalPlayers}</td>
+        </tr>
+        <tr>
+          <td>Total Points:</td>
+          <td>{totalPoints}</td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
+
+Stats.propTypes = {
+  players: React.PropTypes.array.isRequired,
+};
 
 function Header(props) {
   return (
     <div className="header">
+      <Stats players={props.players} />
       <h1>{props.title}</h1>
     </div>
   );
@@ -28,6 +68,7 @@ function Header(props) {
 
 Header.propTypes = {
   title: React.PropTypes.string.isRequired,
+  players: React.PropTypes.array.isRequired,
 };
 
 function Counter(props) {
@@ -96,7 +137,7 @@ onScoreChange: function(index, delta) {
   render: function() {
     return (
       <div className="scoreboard">
-      <Header title={this.props.title} />
+      <Header title={this.props.title} players={this.state.players} />
 
         <div className="players">
           {this.state.players.map(function(player, index) {
@@ -109,6 +150,7 @@ onScoreChange: function(index, delta) {
             );
           }.bind(this))}
         </div>
+        <AddPlayerForm />
       </div>
     );
   }
